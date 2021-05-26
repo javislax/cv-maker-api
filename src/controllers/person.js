e413e63be42fe6da95a328e0de7ef3852a8eb41f
Javier Fernández-Coto Córdoba <javier.fernandez@edu.uah.es>
2021-05-26 23:39:15 +0200

    Anulada verificacion

--- src/controllers/person.js
+++ src/controllers/person.js
@@ -3,13 +3,6 @@ const controller = {};
 const Person = require("../models/person");
 
 controller.savePerson = async (req, res) => {
-  /* const name = req.body.name
-    const surname = req.body.surname
-    const job = req.body.job
-    const biography = req.body.biography
-    const picture = req.body.picture
-    const birthday = req.body.birthday */
-
   const languages = req.body.languages;
   const experience = req.body.experience;
   const education = req.body.education;
@@ -20,12 +13,12 @@ controller.savePerson = async (req, res) => {
   const picture = req.body.picture;
   const birthday = req.body.birthday;
   const job = req.body.job;
-  const iduser = req.user
+  //const iduser = req.user
 
-  /* if (!name || !surname || !job || !biography || !picture || !birthday) {
+   /* if (!name || !surname || !job || !biography || !picture || !birthday || !education || !experience || !languages || !phone) {
         res.status(400).send()
         return
-    } */
+    }  */
     
   try {
     const person = new Person({
